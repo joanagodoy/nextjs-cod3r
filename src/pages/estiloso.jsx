eb9bd82ded7c5f8672b869634ea4f495a12b3da7
joanagodoy <joanapereiragodoy@gmail.com>
2021-12-27 10:30:05 -0300

    Navegacao entre Componentes

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,8 +1,10 @@
 import styles from "../styles/Estiloso.module.css"
+import Link from 'next/Link'
 
 export default function Estiloso(){
     return (
         <div className={styles.roxo}>
+            <Link href='/'>Voltar</Link>
             <h1>Componente Estiloso!</h1>
         </div>
     )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,8 @@
+import Link from 'next/Link'
 export default function Inicio(){
   return (
     <div>
+      <Link href='/estiloso'>Estiloso</Link>
       <h1>Fundamentos Next.js & React</h1>
       <h2>Vamos estudar esse framework</h2>
     </div>
