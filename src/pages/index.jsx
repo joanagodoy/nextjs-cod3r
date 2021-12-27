7303b1f172a6a3a9cff928aa7b9c72eb4ebb4d73
joanagodoy <joanapereiragodoy@gmail.com>
2021-12-27 18:35:26 -0300

    Navegação Dinamica

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,7 @@ export default function Inicio(){
       <Navegeador titulo="Exemplo"  destino="/exemplo" cor="#9400d3"></Navegeador>
       <Navegeador titulo="JSX"      destino="/jsx"      cor="firebrick"></Navegeador>
       <Navegeador titulo="Navegação #01"      destino="/navegacao"      cor="green"></Navegeador>
+      <Navegeador titulo="Navegação #02"      destino="/cliente/sp-1/123"      cor="green"></Navegeador>
     </div>
   )
 }
