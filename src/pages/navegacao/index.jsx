b326e3b7ed4d9620914ca99b7b7797ffcf109e24
joanagodoy <joanapereiragodoy@gmail.com>
2021-12-27 18:22:16 -0300

    Exemplo de Navegação Simples

---
+++ src/pages/navegacao/index.jsx
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout.jsx'
+
+export default function Navegacao(){
+    return (
+        <Layout titulo="Navegação #01">
+            <h1>Navegação #01</h1>
+        </Layout>
+    )
+}
